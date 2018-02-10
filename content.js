19112dbc31645959b6a7d33dd06625068ceed000
Elizabeth Worth <elizabethworth@Elizabeths-MacBook-Pro.local>
2018-02-10 15:24:05 -0800

    Trying to get back on track

--- content.js
+++ content.js
@@ -1,9 +1,19 @@
 //This currently is using the Epicurious website class name//
-var ingredients = document.getElementsByClassName("ingredient");
-  for(var i = 0; i < ingredients.length; i++){
-    var newText = ingredients[i].textContent.replace(/(?:[1-9][0-9]*|0)(?:\/[1-9][0-9]*)?/g, function(word){
-      return parseInt(word)/2;
-// the integer 2 will soon be replaced with a "serving input number" to create a ratio for the output number      
-    })
-    ingredients[i].textContent = newText;
-  }
+function convertIngredients(){
+  var oldYield = parseInt(document.getElementById("old_yield").value);
+  var newYield = parseInt(document.getElementById("new_yield").value);
+  var ingredients = document.getElementsByClassName("ingredient");
+    for(var i = 0; i < ingredients.length; i++){
+      var newText = ingredients[i].textContent.replace(/(?:[1-9][0-9]*|0)(?:\/[1-9][0-9]*)?/g, function(word){
+        return parseInt(word)*(newYield/oldYield);
+  // the integer 2 will soon be replaced with a "serving input number" to create a ratio for the output number
+      })
+      ingredients[i].textContent = newText;
+    }
+}
+document.addEventListener("DOMContentLoaded", function(){
+  document.getElementById("update").addEventListener("click", function(e){
+    e.preventDefault();
+    convertIngredients();
+  });
+});
